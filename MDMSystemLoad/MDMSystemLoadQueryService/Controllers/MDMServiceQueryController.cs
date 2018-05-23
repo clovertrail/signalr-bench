c4c2b642b4ed290f4e15fd5d8d46c0ffa3407811
Hongjiang Zhang <honzhan@microsoft.com>
2018-05-23 18:35:04 +0800

    add REST API to query MDM Metrics

---
+++ MDMSystemLoad/MDMSystemLoadQueryService/Controllers/MDMServiceQueryController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace MDMSystemLoadQueryService.Controllers
+{
+    [Route("mdm")]
+    public class MDMServiceQueryController : Controller
+    {
+        private IMDMQuery _mDMQuery;
+
+        public MDMServiceQueryController(IMDMQuery mDMQuery)
+        {
+            _mDMQuery = mDMQuery;
+        }
+
+        // GET mdm/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET mdm/query?platform=
+        [HttpGet, Route("query")]
+        public string Get(string platform, string systemLoad, string podName, string dateStart, string dateEnd)
+        {
+            if (DateTime.TryParse(dateStart, out DateTime normalDateStart))
+            {
+                Console.WriteLine($"Successfully parse the start date: {normalDateStart}");
+            }
+            else
+            {
+                Console.WriteLine($"Fail to parse the start date: {dateStart}");
+            }
+            if (DateTime.TryParse(dateEnd, out DateTime normalDateEnd))
+            {
+                Console.WriteLine($"Successfully parse the end date: {normalDateEnd}");
+            }
+            else
+            {
+                Console.WriteLine($"Fail to parse the end date: {dateEnd}");
+            }
+            if (!Enum.TryParse<PlatformType>(platform, out var platformType))
+            {
+                Console.WriteLine($"Fail to parse platform type {platform}");
+            }
+            if (!Enum.TryParse<SystemLoadType>(systemLoad, out var systemLoadType))
+            {
+                Console.WriteLine($"Fail to parse SystemLoad type {systemLoad}");
+            }
+            Console.WriteLine($"Query metrics for {platformType}, {systemLoadType}, {podName}, {normalDateStart}, {normalDateEnd}");
+            return _mDMQuery.QueryMetrics(platformType, systemLoadType, podName, dateStart, dateEnd);
+        }
+
+        // POST mdm/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT mdm/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE mdm/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
