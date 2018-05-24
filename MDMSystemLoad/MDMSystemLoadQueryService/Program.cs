41fd75f7bf88c951e9cf6aa978a1ac30aa20fc39
Hongjiang Zhang <honzhan@microsoft.com>
2018-05-24 17:51:19 +0800

    refactor

--- MDMSystemLoad/MDMSystemLoadQueryService/Program.cs
+++ MDMSystemLoad/MDMSystemLoadQueryService/Program.cs
@@ -20,6 +20,7 @@ namespace MDMSystemLoadQueryService
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+		.UseUrls("http://*:5353")
                 .Build();
     }
 }
