507ab3779b43e73b99c70a3b2e3b6e71830f6f6b
Hongjiang Zhang <honzhan@microsoft.com>
2018-06-10 15:39:11 +0000

    fix report bug

--- jenkins-signalr-service-on-vm.sh
+++ jenkins-signalr-service-on-vm.sh
@@ -93,4 +93,4 @@ multiple_try_run $ServiceHost $BaseConnectionNumber $ConnectionSteps $Concurrent
 sh gen_all_units.sh # gen_all_report
 sh publish_report.sh 
 sh gen_summary.sh # refresh summary.html in NginxRoot gen_summary
-sh send_mail.sh $HOME/NginxRoot/$result_root/all.html
+sh send_mail.sh $HOME/NginxRoot/$result_root/allunits.html
