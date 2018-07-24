c2ca4bc1ae75cad8ad37113316ea798af5888b9c
Hongjiang Zhang <honzhan@microsoft.com>
2018-07-24 09:51:51 +0000

    update report

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -7,4 +7,4 @@ create_root_folder
 sh gen_all_report.sh # gen_all_report
 sh publish_report.sh 
 sh gen_summary.sh # refresh summary.html in NginxRoot gen_summary
-sh send_mail.sh $HOME/NginxRoot/$result_root/all.html
+sh send_mail.sh $HOME/NginxRoot/$result_root/allunits.html
