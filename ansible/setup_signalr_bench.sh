46d2b4c58837ea29d053e713a774a159c5c8eaa0
Hongjiang Zhang <honzhan@microsoft.com>
2018-07-25 02:54:24 +0000

    remove the unnecessary update

--- ansible/setup_signalr_bench.sh
+++ ansible/setup_signalr_bench.sh
@@ -4,7 +4,6 @@ ansible-playbook -i signalr_hosts install_config_go.yaml
 ansible-playbook -i signalr_hosts git_clone_websocket.yaml
 ansible-playbook -i signalr_hosts build_websocket.yaml
 ansible-playbook -i signalr_hosts git_clone_azuresignalrchatsample.yaml
-ansible-playbook -i signalr_hosts set_hostname_azuresignalrchatsample.yaml
 ansible-playbook -i signalr_hosts install_package.yaml
 ansible-playbook -i signalr_hosts install_kubectl.yaml
 ansible-playbook -i signalr_hosts build_azuresignalrsample.yaml
