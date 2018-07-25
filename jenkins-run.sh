10f01d1f6a890c7fca7ae877ccfe84423ed5bf1c
Hongjiang Zhang <honzhan@microsoft.com>
2018-07-25 02:22:50 +0000

    send tab format report

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -4,7 +4,5 @@
 create_root_folder
 
 . ./jenkins-run-websocket.sh
-sh gen_all_report.sh # gen_all_report
-sh publish_report.sh 
-sh gen_summary.sh # refresh summary.html in NginxRoot gen_summary
-sh send_mail.sh $HOME/NginxRoot/$result_root/allunits.html
+
+gen_final_report
